3c2bb759d2be1dfdaa91a5956a847a505fbbbfe5
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-16 12:43:51 +0530

    adding sg_description for eks module

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -55,7 +55,7 @@ resource "aws_security_group_rule" "ingress_alb_https" {
     to_port = 443
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = module.ingress_alb_sg
+    security_group_id = module.ingress_alb_sg.id
 }
 
 resource "aws_security_group_rule" "node_ingress_alb" {
@@ -63,8 +63,8 @@ resource "aws_security_group_rule" "node_ingress_alb" {
     from_port = 30000
     to_port = 32767
     protocol = "tcp"
-    source_security_group_id = module.ingress_alb_sg
-    security_group_id = module.node_sg
+    source_security_group_id = module.ingress_alb_sg.id
+    security_group_id = module.node_sg.id
 }
 
 resource "aws_security_group_rule" "node_eks_control_plane" {
@@ -82,7 +82,7 @@ resource "aws_security_group_rule" "node_vpc" {
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["10.0.0.0/16"]
-    security_group_id = module.node_sg
+    security_group_id = module.node_sg.id
 }
 
 resource "aws_security_group_rule" "node_bastion" {
@@ -90,8 +90,8 @@ resource "aws_security_group_rule" "node_bastion" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    source_security_group_id = module.bastion_sg
-    security_group_id = module.node_sg
+    source_security_group_id = module.bastion_sg.id
+    security_group_id = module.node_sg.id
 }
 
 resource "aws_security_group_rule" "eks_control_plane_node" {
@@ -99,8 +99,8 @@ resource "aws_security_group_rule" "eks_control_plane_node" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    source_security_group_id = module.node_sg
-    security_group_id = module.eks_control_plane_sg
+    source_security_group_id = module.node_sg.id
+    security_group_id = module.eks_control_plane_sg.id
 }
 
 resource "aws_security_group_rule" "eks_control_plane_bastion" {
@@ -108,8 +108,8 @@ resource "aws_security_group_rule" "eks_control_plane_bastion" {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    source_security_group_id = module.bastion_sg
-    security_group_id = module.eks_control_plane_sg
+    source_security_group_id = module.bastion_sg.id
+    security_group_id = module.eks_control_plane_sg.id
 }
 
 resource "aws_security_group_rule" "mysql_bastion" {
@@ -117,8 +117,8 @@ resource "aws_security_group_rule" "mysql_bastion" {
     from_port = 3306
     to_port = 3306
     protocol = "tcp"
-    source_security_group_id = module.bastion_sg
-    security_group_id = module.mysql_sg
+    source_security_group_id = module.bastion_sg.id
+    security_group_id = module.mysql_sg.id
 }
 
 resource "aws_security_group_rule" "mysql_node"{
@@ -126,8 +126,8 @@ resource "aws_security_group_rule" "mysql_node"{
     from_port = 3306
     to_port = 3306
     protocol = "tcp"
-    source_security_group_id = module.node_sg
-    security_group_id = module.mysql_sg
+    source_security_group_id = module.node_sg.id
+    security_group_id = module.mysql_sg.id
 }
 
 resource "aws_security_group_rule" "bastion_public" {
@@ -136,5 +136,5 @@ resource "aws_security_group_rule" "bastion_public" {
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    security_group_id = module.bastion_sg
+    security_group_id = module.bastion_sg.id
 }
