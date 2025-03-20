75806829a54240f293bc4f87a826262f8aa1fd0a
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-20 12:32:30 +0530

    creating data sources

--- 40-eks/data.tf
+++ 40-eks/data.tf
@@ -7,7 +7,7 @@ data "aws_ssm_parameter" "node_sg_id" {
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
-    name = "/${var.project_name}/${var.environment}-private_sg_ids"
+    name = "/${var.project_name}/${var.environment}-private_subnet_ids"
 }
 
 data "aws_ssm_parameter" "vpc_id" {
