2035804156d26beaf9bc1a211926913d6d273cbc
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-20 13:35:22 +0530

    creating main.tf,variables.tf,provider.tf,locals.tf,parameters.tf

--- 50-acm/parameters.tf
+++ 50-acm/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "https_certificate_arn" {
     name = "/${var.project_name}/${var.environment}/https_certificate_arn"
     type = "String"
-    values = aws_acm_certificate.roboshop.arn
+    value = aws_acm_certificate.roboshop.arn
 }
